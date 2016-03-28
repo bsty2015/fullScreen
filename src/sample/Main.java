44dd3f6bad445c69ef9b5a2eb15f8a5ae9411e05
bsty2015 <luzhao186@163.com>
2016-03-28 23:41:05 +0800

    add

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,9 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.*;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.*;
 import javafx.stage.Stage;
 
 import java.awt.*;
@@ -15,22 +22,37 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        Scene root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 //        primaryStage.setTitle("Hello World");
+        StackPane p = new StackPane();
+        p.setId("pane");
+        Scene root = new Scene(p);
+        Text text = new Text("我中毒了::>_<::,请勿动乱动,尽快联系管理员!!!");
+        text.setFont(javafx.scene.text.Font.font("微软雅黑",25));
         Toolkit toolkit = Toolkit.getDefaultToolkit();
-        primaryStage.setScene(new Scene(root, toolkit.getScreenSize().width, toolkit.getScreenSize().height));
+        double width = toolkit.getScreenSize().width;
+        double height = toolkit.getScreenSize().height;
+        primaryStage.setScene(root);
         primaryStage.setFullScreen(true);
+        primaryStage.setAlwaysOnTop(true);
         primaryStage.setFullScreenExitHint("");
         primaryStage.setFullScreenExitKeyCombination(new KeyCombination() {
             @Override
             public boolean match(KeyEvent event) {
-                if (event.getCode() == KeyCode.ALT)
-                    return super.match(event);
-                else
-                    return false;
+//                if (event.getCode() == KeyCode.A)
+//                    return super.match(event);
+//                else
+                return false;
             }
         });
+        root.getStylesheets().addAll(this.getClass().getResource("main.css").toExternalForm());
         primaryStage.show();
+        Thread.sleep(3000);
+//        Scene scene = new Scene(p, width, height);
+        root.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
+        primaryStage.setScene(root);
+        primaryStage.show();
+
     }
 
 
